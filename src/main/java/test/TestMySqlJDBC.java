4b29c87e0fcbfa5806c3a355409f999805b92756
aprendiztic1 <aprendiztic1@MAXTI003.CERAMIGRESSA.LOCAL>
2024-08-22 16:59:44 -0500

    conexion bd java

---
+++ src/main/java/test/TestMySqlJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author aprendiztic1
+ */
+public class TestMySqlJDBC {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone";
+    }
+   
+}
